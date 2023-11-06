22eaf0876e99e5f09cf482db40c9ef4a671394ad
Crowdbotics <team@crowdbotics.com>
2023-11-06 16:29:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pharmacy-lite-templ-44371",
+  url: "https://pharmacy-lite-templ-44371.botics.co",
+  api: "https://pharmacy-lite-templ-44371.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
