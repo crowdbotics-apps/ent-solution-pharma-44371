7770d67bf2866ff8b649c2c7dfcf5a51b235ed02
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-11-17 17:21:05 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "pharmacy-lite-templ-44371",
-  url: "https://pharmacy-lite-templ-44371.botics.co",
-  api: "https://pharmacy-lite-templ-44371.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "pharmacy-lite-templ-44371",
+  "url": "https://pharmacy-lite-templ-44371.botics.co",
+  "api": "https://pharmacy-lite-templ-44371.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "splash"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
